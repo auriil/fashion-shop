48b8514abf53ff2f8c782d7b51e20e3705bbab32
auriil <andruxach@gmail.com>
2019-07-03 00:06:18 +0300

    create slider and mixin for button

--- app/js/common.js
+++ app/js/common.js
@@ -1,12 +1,24 @@
 (function($) {
 
-    $("#owl-demo").owlCarousel({
+    $(".slider .owl-carousel").owlCarousel({
         loop:true,
         nav: true,
+        dots: false,
         slideSpeed : 300,
         paginationSpeed : 400,
         singleItem:true,
-        items : 1
+        items : 1,
+        navText: [],
+        responsive: {
+            0: {
+                items: 1,
+                dots: true,
+                nav: false
+            },
+            480: {
+                items: 1
+            }
+        }
     });
 
 })(jQuery);
