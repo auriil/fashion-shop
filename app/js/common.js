938b72b9d2a1984a95a67c7177d8d07ab4e0094b
auriil <andruxach@gmail.com>
2019-07-03 23:20:37 +0300

    create featured

--- app/js/common.js
+++ app/js/common.js
@@ -21,4 +21,32 @@
         }
     });
 
+    $(".carousel").owlCarousel({
+        loop:true,
+        nav: true,
+        dots: false,
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true,
+        items : 1,
+        margin: 30,
+        navText: [],
+        responsive: {
+            0: {
+                items: 1,
+                dots: true,
+                nav: false
+            },
+            480: {
+                items: 2
+            },
+            680: {
+                items: 3
+            },
+            1024: {
+                items: 4
+            }
+        }
+    });
+
 })(jQuery);
